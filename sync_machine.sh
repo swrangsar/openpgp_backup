854b146ad08d599b6887a1357ddb47201773414a
swrangsar basumatary <swrangsar@pm.me>
2020-09-01 07:54:06 +0530

    refactor shell script

--- sync_machine.sh
+++ sync_machine.sh
@@ -4,13 +4,7 @@ sync_git_repo() {
 	REPODIR="$1"
 	
 	echo "syncing repo ${REPODIR}..."
-	if [ -d "${REPODIR}" ]; then
-		cd "${REPODIR}" || (echo "warn: cd ${REPODIR} failed"; return)
-		git fetch
-		git push
-	else
-		echo "warn: could not sync repo ${REPODIR}"
-	fi
+	cd "${REPODIR}" && git fetch && git push
 }
 
 gpg --refresh-keys --keyserver hkps://keys.openpgp.org
