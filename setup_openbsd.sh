cb4ee107e73402a355d95dd5c9decdf8475e0367
swrangsar basumatary <swrangsar@cock.li>
2020-10-25 19:50:05 +0530

    add noice

--- setup_openbsd.sh
+++ setup_openbsd.sh
@@ -5,4 +5,4 @@ sysupgrade && syspatch
 pkg_add -v rsync syncthing links+
 pkg_add -v pstree neofetch newsboat neomutt git gnupg
 pkg_add -v dwm dmenu st sic sxiv feh mpv curl password-store
-pkg_add -v surf
+pkg_add -v surf noice fdm
