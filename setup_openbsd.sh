e4aaaf445ff7bcfefc09cba237e696cbcc2a7dc3
swrangsar basumatary <swrangsar@cock.li>
2020-10-25 20:07:44 +0530

    update package add

--- setup_openbsd.sh
+++ setup_openbsd.sh
@@ -2,7 +2,9 @@
 
 syspatch; fw_update; sysmerge;
 sysupgrade && syspatch
-pkg_add -v rsync syncthing links+
+pkg_add -v rsync neovim syncthing links+
 pkg_add -v pstree neofetch newsboat neomutt git gnupg
 pkg_add -v dwm dmenu st sic sxiv feh mpv curl password-store
-pkg_add -v surf noice fdm
+pkg_add -v maim surf noice fdm sxhkd zathura
+pkg_add -v zathura-ps zathura-pdf-mupdf zathura-djvu youtube-dl
+pkg_add -v ImageMagick
