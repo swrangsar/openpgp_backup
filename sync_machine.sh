db7a21edac28f4f687b39fc70c25a98f4a0d1071
swrangsar basumatary <swrangsar@cock.li>
2020-10-29 08:53:23 +0530

    rsync dotfiles

--- sync_machine.sh
+++ sync_machine.sh
@@ -12,7 +12,15 @@ gpg2 --refresh-keys --keyserver hkps://keys.openpgp.org
 fdm -vv fetch
 
 
-sync_git_repo $HOME/repos/openpgp_backup/
+
+# sync_git_repo $HOME/repos/openpgp_backup/
+echo "syncing openpgp_backup..."
+cd $HOME/repos/openpgp_backup \
+	&& git pull \
+	&& rsync -axPuv $HOME/.config/sxhkd/sxhkdrc $HOME/repos/openpgp_backup/.config/sxhkd/ \
+	&& rsync -axPuv $HOME/.config/youtube-dl/config $HOME/repos/openpgp_backup/.config/youtube-dl/ \
+	&& git commit -am "dotfiles update" \
+	&& git push
 sync_git_repo $HOME/repos/backup_gpg/
 
 pass git pull && pass git push
