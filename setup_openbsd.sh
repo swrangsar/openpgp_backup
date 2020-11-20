4d0d00651ce393bd0e88b0bbbe736733c95939b3
swrangsar basumatary <swrangsar@pm.me>
2020-11-20 12:15:11 +0530

    dotfile update

--- setup_openbsd.sh
+++ setup_openbsd.sh
@@ -8,4 +8,4 @@ pkg_add -v dwm dmenu st sic sxiv feh mpv curl password-store
 pkg_add -v maim surf noice fdm sxhkd zathura
 pkg_add -v zathura-ps zathura-pdf-mupdf zathura-djvu youtube-dl
 pkg_add -v ImageMagick wordnet pass-otp zbar
-pkg_add -v transmission sc scim
+pkg_add -v transmission sc scim abook
