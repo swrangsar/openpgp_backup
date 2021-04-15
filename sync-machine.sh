8e250b891449d367ca1ca8db1e551fd9f0f8e128
swrangsar basumatary <swrangsar@disroot.org>
2021-04-15 17:52:13 +0530

    archive bsd scripts

--- sync-machine.sh
+++ sync-machine.sh
@@ -9,7 +9,7 @@ cd $HOME/repos/openpgp_backup &&
 	git pull &&
 	echo "rsync dotfiles..." &&
 	rsync -axPuv $HOME/.config/sxhkd/sxhkdrc $HOME/repos/openpgp_backup/.config/sxhkd/ &&
-	rsync -axPuv $HOME/.local/bin/sb-* $HOME/repos/openpgp_backup/.local/bin &&
+	rsync -axPuv $HOME/.local/bin/{sb-mailbox} $HOME/repos/openpgp_backup/.local/bin &&
 	git commit -am "dotfile update" &&
 	git status &&
 	git push
