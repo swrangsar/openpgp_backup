910dcb33c6ed00df6d7541459cf5a4b38338d556
swrangsar basumatary <swrangsar@disroot.org>
2021-04-15 22:35:01 +0530

    formula invest script

---
+++ finance/formula-invest.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# formula file format
+# stock	RoA P/E
+#
+# algo: magic formula investing
+# 
+# author: swrangsar basumatary
+# license: GPLv3
+
+[ -n "$1" ] ||
+	( echo "Usage: " $0 " <filename>"; exit 1 )
+
+filename="$1"
+head -1 $filename
+
+awk -F, '(NR > 1) && $2 > 25.0' $filename | sort -t , -nk 3
