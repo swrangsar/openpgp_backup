48874fbbe872d15f4a1e12b21452332c9360748c
swrangsar basumatary <swrangsar@disroot.org>
2021-04-15 17:46:32 +0530

    dotfile update

--- sync-machine.sh
+++ sync-machine.sh
@@ -3,3 +3,13 @@
 gpg --refresh-keys --keyserver hkps://keys.openpgp.org
 
 fdm -vv fetch
+
+echo "syncing openpgp_backup..."
+cd $HOME/repos/openpgp_backup &&
+	git pull &&
+	echo "rsync dotfiles..." &&
+	rsync -axPuv $HOME/.config/sxhkd/sxhkdrc $HOME/repos/openpgp_backup/.config/sxhkd/ &&
+	rsync -axPuv $HOME/.local/bin/sb-* $HOME/repos/openpgp_backup/.local/bin &&
+	git commit -am "dotfile update" &&
+	git status &&
+	git push
