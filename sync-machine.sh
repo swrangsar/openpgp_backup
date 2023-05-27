872f4190790e8bea0bf87adffae2fadca12b51ca
swrangsar basumatary <swrangsar@disroot.org>
2023-05-27 18:31:29 +0530

    clean up

--- sync-machine.sh
+++ sync-machine.sh
@@ -8,16 +8,8 @@ pkill -RTMIN+12 dwmblocks
 echo "syncing openpgp_backup..."
 cd $HOME/repos/openpgp_backup &&
 	git pull &&
-	echo "rsync dotfiles..." &&
-	rsync -axPuv $HOME/.config/sxhkd/sxhkdrc $HOME/repos/openpgp_backup/.config/sxhkd/ &&
-	rsync -axPuv $HOME/.local/bin/sb-forecast $HOME/repos/openpgp_backup/.local/bin/ &&
-	rsync -axPuv $HOME/.local/bin/sb-mailbox $HOME/repos/openpgp_backup/.local/bin/ &&
-	rsync -axPuv $HOME/.local/bin/transadd $HOME/repos/openpgp_backup/.local/bin/ &&
-	git commit -am "dotfile update" &&
 	git status &&
 	git push
 
-## [ -d $HOME/.password-store ] && [ -d /media/password-store ] &&
-	rsync -axPuv $HOME/.password-store/  $HOME/password-store
 
 pkill -RTMIN+5 dwmblocks
